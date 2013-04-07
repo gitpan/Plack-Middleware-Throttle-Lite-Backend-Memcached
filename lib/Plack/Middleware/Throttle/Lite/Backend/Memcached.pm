ac3c6f10b6f8f1d92ee4d5e9a6f3451c3ffbf838
Anton Gerasimov <chim@cpan.org>
2013-04-07 13:38:39 +0000

    Import of CHIM/Plack-Middleware-Throttle-Lite-Backend-Memcached-0.02 from CPAN.
    gitpan-cpan-distribution: Plack-Middleware-Throttle-Lite-Backend-Memcached
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         CHIM/Plack-Middleware-Throttle-Lite-Backend-Memcached-0.02.tar.gz
    gitpan-cpan-author:       CHIM
    gitpan-cpan-maturity:     released

--- lib/Plack/Middleware/Throttle/Lite/Backend/Memcached.pm
+++ lib/Plack/Middleware/Throttle/Lite/Backend/Memcached.pm
@@ -8,7 +8,7 @@ use Carp ();
 use parent 'Plack::Middleware::Throttle::Lite::Backend::Abstract';
 use Cache::Memcached::Fast;
 
-our $VERSION = '0.01'; # VERSION
+our $VERSION = '0.02'; # VERSION
 our $AUTHORITY = 'cpan:CHIM'; # AUTHORITY
 
 __PACKAGE__->mk_attrs(qw(mc));
@@ -46,7 +46,7 @@ Plack::Middleware::Throttle::Lite::Backend::Memcached - Memcache-driven storage 
 
 =head1 VERSION
 
-version 0.01
+version 0.02
 
 =head1 DESCRIPTION
 
